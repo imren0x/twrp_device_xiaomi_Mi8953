b5e5cdab01901a2f438e2d4f1271c8d9a3ba104f
Mika <enashinonome264@gmail.com>
2026-08-21 08:18:58 +0000

    Mi8953: add erofs support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,10 @@ TARGET_KERNEL_VERSION := 4.9
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_PREBUILT_KERNEL := device/xiaomi/Mi8953/prebuilt/Image.gz-dtb
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
