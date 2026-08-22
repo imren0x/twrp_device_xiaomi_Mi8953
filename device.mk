b826d0f30a05c21a0c3b2ccc7f330a4d46c80866
Mika <enashinonome264@gmail.com>
2026-08-22 07:46:28 +0000

    Mi8953: remove fastbootd

--- device.mk
+++ device.mk
@@ -26,25 +26,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.boot.dynamic_partitions=true \
-	ro.boot.dynamic_partitions_retrofit=true
-
-# Fastbootd
-TW_INCLUDE_FASTBOOTD := true
-PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-service \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.fastbootd.available=true
-
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/vintf/manifest.xml:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/etc/vintf/manifest.xml
 
 # Soong namespaces
